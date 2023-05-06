cdb2f3b4cd5bd1ce3e06d69ddea0ac5a08a9630b
Raiven <825728964@qq.com>
2023-05-06 21:40:14 +0800

    DTO

---
+++ TMV.DTO_old/Users/UsersDTO.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMV.DTO.ModelData;
+
+namespace TMV.DTO.Users
+{
+ 
+
+    public class Request_Users : ModelDTO
+    {
+        /// <summary>
+        /// 用户名称
+        /// </summary> 
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 用户类型-1.管理，2.值班
+        /// </summary> 
+        public string Type { get; set; }
+    }
+    public class UsersModel
+    {
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 用户名称
+        /// </summary> 
+        public string Name { get; set; }
+        /// <summary>
+        /// 用户密码
+        /// </summary> 
+        public string Pwd { get; set; }
+         
+
+        /// <summary>
+        /// 用户类型-1.管理，2.值班
+        /// </summary> 
+        public int Type { get; set; }
+    }
+
+    public class UsersDTO
+    {
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 用户名称
+        /// </summary> 
+        public string Name { get; set; }
+        /// <summary>
+        /// 用户密码
+        /// </summary> 
+        public string Pwd { get; set; }
+
+        /// <summary>
+        /// 注册时间
+        /// </summary> 
+        public DateTime AddTime { get; set; }
+
+        /// <summary>
+        /// 用户类型-1.管理，2.值班
+        /// </summary> 
+        public int Type { get; set; }
+
+    }
+}
---
+++ TMV.DTO_old/Users/UsersPageDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMV.DTO.Users
+{
+    public class UsersPageDto:PageBase
+    {
+        public string Name { get; set; }
+    }
+}
