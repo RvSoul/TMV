50e9bb1d5c667c9ca56793188a798911e69aa7ca
样 赵 <625635095@qq.com>
2023-05-10 00:50:04 +0800

    zy

---
+++ TMV.PrintServer/TMV.PrintServer/WordUtil.cs
@@ -0,0 +1,23 @@
+﻿using NPOI.XWPF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMV.PrintServer
+{
+    public class WordUtil
+    {
+        XWPFDocument document = null;
+        public void WordWrite(string word) 
+        {
+            var path = Application.StartupPath+ @"\printmodel.doc";
+            using (var file=File.OpenRead(path))
+            {
+                document = new XWPFDocument(file);
+            }
+                
+        }
+    }
+}
