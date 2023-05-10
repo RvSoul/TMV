0423e0d33f45362e9e383b2c00beff4db2849a96
样 赵 <625635095@qq.com>
2023-05-10 23:05:10 +0800

    zy

--- TMV.Web/Program.cs
+++ TMV.Web/Program.cs
@@ -1,5 +1,5 @@
 //Serve.Run(RunOptions.Default.WithArgs(args));
-Serve.Run(RunOptions.Default.ConfigureBuilder(builder =>
+Serve.Run(RunOptions.Default.WithArgs(args).ConfigureBuilder(builder =>
 {
-    builder.WebHost.UseUrls("https://localhost:5000");  // 也可以通过 builder.Configuration 读取 urls 配置
+    builder.WebHost.UseUrls("http://localhost:5000");  // 也可以通过 builder.Configuration 读取 urls 配置
 }));
