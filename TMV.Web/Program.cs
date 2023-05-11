1b550a8058bbaabe85d9d280f2c0f42e3d8caca5
样 赵 <625635095@qq.com>
2023-05-11 21:49:26 +0800

    zy

--- TMV.Web/Program.cs
+++ TMV.Web/Program.cs
@@ -1,9 +1,9 @@
-Serve.Run(RunOptions.Default.WithArgs(args));
+//Serve.Run(RunOptions.Default.WithArgs(args));
 //Serve.Run(RunOptions.Default.WithArgs(args).ConfigureBuilder(builder =>
 //{
 //    builder.WebHost.UseUrls("http://localhost:5000");  // 也可以通过 builder.Configuration 读取 urls 配置
 //}));
-//var builder = WebApplication.CreateBuilder(args).Inject();
-//builder.WebHost.UseUrls("http://*:5000");
-//var app = builder.Build();
-//app.Run();
+var builder = WebApplication.CreateBuilder(args).Inject();
+builder.WebHost.UseUrls("http://*:5000");
+var app = builder.Build();
+app.Run();
