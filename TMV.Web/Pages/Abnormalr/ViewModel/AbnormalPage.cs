7ffd32797ef6c7828f697b65ba7de807e060643b
Raiven <825728964@qq.com>
2023-05-14 01:47:12 +0800

    zy

---
+++ TMV.Web/Pages/Abnormalr/ViewModel/AbnormalPage.cs
@@ -0,0 +1,16 @@
+﻿using TMV.DTO.Abnormal;
+using TMV.DTO.AbnormalRecords;
+
+namespace TMV.Web.Pages.Abnormalr.ViewModel
+{
+	public class AbnormalPage: PageBase<AbnormalDTO>
+	{
+		public string Code { get; set; }
+		public AbnormalPage() { }
+		public AbnormalPage(List<AbnormalDTO> datas)
+		{
+			Datas = new List<AbnormalDTO>();
+			Datas.AddRange(datas);
+		}
+	}
+}
